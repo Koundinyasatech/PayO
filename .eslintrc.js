6eab5d164999d77bbe4d49e11e116a1e08d08f3a
unknown <sowmya.ch@koundinyasatech.com>
2026-05-05 18:26:48 +0530

    profile and transaction screens and send again butto progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,4 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
-  requireConfigFile: false,
-
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-
-  env: {
-    es6: true,
-    node: true,
-  },
-
-  plugins: ['react'],
-
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-
-  rules: {
-    'react/react-in-jsx-scope': 'off', // for React 17+
-  },
+  extends: '@react-native',
 };
